62dd525ab0a523f5f3e08605c87a9b79b8424e88
SharkGamerYT <93350857+SharkGamerYT@users.noreply.github.com>
2022-11-25 17:47:41 -0600

    Update config.js

--- config.js
+++ config.js
@@ -4,10 +4,11 @@ import firebase from 'firebase';
 var firebaseConfig = {
 apiKey: "AIzaSyACjmC0-vwl8HhPUIeZc2rdh-dmQc68XTQ",
   authDomain: "votacion-6d00d.firebaseapp.com",
+  databaseURL: "https://votacion-6d00d-default-rtdb.firebaseio.com",
   projectId: "votacion-6d00d",
   storageBucket: "votacion-6d00d.appspot.com",
   messagingSenderId: "1034967849705",
-  appId: "1:1034967849705:web:51c890c300f0026e7c2e78"
+  appId: "1:1034967849705:web:3f9fb3da6986e0c07c2e78"
 };
 // Inicializar Firebase
 firebase.initializeApp(firebaseConfig);
